4d63a023f1c58fb4ff5e24923c4d9d497f2713bf
Gil Rodarte <yepes@YepesMac.local>
2018-01-14 21:52:59 -0600

    CoreDataToDo

---
+++ CoreDataToDo/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  CoreDataToDo
+//
+//  Created by Gil Rodarte on 14/01/18.
+//  Copyright © 2018 Gil Rodarte. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UITableViewController {
+    
+    var items: [NSManagedObject] = []
+    let cellId = "cellId"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        navigationController?.navigationBar.barTintColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
+        navigationController?.navigationBar.tintColor = UIColor.white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addItem))
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Item")
+        do {
+            items = try managedContext.fetch(fetchRequest)
+        } catch let err as NSError {
+            print("Error al recuperar datos", err)
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.value(forKey: "itemName") as? String
+        return cell
+    }
+    
+    func save(_ itemName: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Item", in: managedContext)!
+        let item = NSManagedObject(entity: entity, insertInto: managedContext)
+        item.setValue(itemName, forKey: "itemName")
+        
+        do {
+            try managedContext.save()
+            items.append(item)
+        } catch let err as NSError {
+            print("Error al intentar guardar los datos", err)
+        }
+        
+    }
+    
+    @objc func addItem(_ sender: AnyObject) {
+        let alertController = UIAlertController(title: "Añadir a lista", message: "Introduce lo que quieres", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Guardar", style: .default) { [unowned self] action in
+            guard let textField = alertController.textFields?.first, let itemToAdd = textField.text else { return }
+            self.save(itemToAdd)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .destructive, handler: nil)
+        alertController.addTextField(configurationHandler: nil)
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
+
